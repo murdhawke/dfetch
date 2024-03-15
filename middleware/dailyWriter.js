40b1a27a8862abaf09482f35574d8de8565e5ba8
murdhawke <amoscheruiyot22@gmail.com>
2024-03-16 00:44:01 +0300

    Copy query

--- middleware/dailyWriter.js
+++ middleware/dailyWriter.js
@@ -4,27 +4,29 @@ const dailyData = require('../modules/daily.js');
 let converter = require('json-2-csv');
 const fs = require('fs');
 const moment =  require('moment');
+const { Client } = require('pg');
     through = require('through'),
     split = require('split');
+
+
+const todayDate = new Date();
+const formattedDate = moment(todayDate).format('DD-MM-YYYY '); // use moment.js to format the date to dd-mm-yyyy.
+
 
 async function writeDaily() {
   const data = await dailyData.saveData();
   const csv = converter.json2csv(data);
-  const todayDate = new Date();
-  const formattedDate = moment(todayDate).format('DD-MM-YYYY '); // use moment.js to format the date to dd-mm-yyyy.
-  /*
   fs.writeFile(__dirname + '/../data/daily' + ' ' + formattedDate + '.csv', csv, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
-*/
 const inFile = fs.createReadStream('../data/daily' + ' ' + formattedDate + '.csv');
     outFile = fs.createWriteStream('./out');
 let headers;
 
-const th = through(function (data) {
+const th = through(function (data) { 
   if (typeof headers === "undefined") {
-    headers = data;
+    headers = data; // check for headers in the csv file.
     th.pause();
     setTimeout(function () { th.resume(); }, 5000);
   } else {
@@ -36,13 +38,29 @@ inFile.pipe(split())
   .pipe(th)
   .pipe(outFile)
   .on("close", function () {
-    console.log("had headers: " + headers);
+    console.log("had headers: " + headers); // show headers
   });
 }
 
-writeDaily();
+async function copyQuery(){
+
+    const client = new Client({
+      host: 'localhost',
+      port: 5432,
+      user: 'postgres',
+      password: 'amos1023',
+      database: 'forex',
+    });
+    await client.connect();
+    const query = "COPY rates FROM '/home/amos/forex/data/daily" + ' ' + formattedDate + ".csv' DELIMITER ',' CSV HEADER";
+    console.log('Connected to PostgreSQL database');
+    return client;
+}
+// writeDaily(); // run the primary function
+copyQuery()
 
 
 module.exports = {
-    writeDaily
+    writeDaily,
+    copyQuery
 };
